94adae7fa0c992e46285157ab7cd1bd415f6d30d
Mugiwara52 <mugiwara52@mail.ru>
2024-12-26 21:52:36 +0300

    sss

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import { FFmpegExecutor } from "./commands/ffmpeg/ffmpeg.executor";
+import { ConsoleLogger } from "./out/console-logger/console-logger";
+
+export class App {
+    async run() {
+        new FFmpegExecutor(ConsoleLogger.getInstance()).execute();       
+    }
+}
+
+const app = new App();
+app.run();
